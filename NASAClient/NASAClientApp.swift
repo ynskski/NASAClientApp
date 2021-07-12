8ea99fac92a9bd72abdbe3eaf90b5d55cba05664
Yunosuke Sakai <qmkns246@gmail.com>
2021-07-12 15:09:26 +0900

    Initial Commit

---
+++ NASAClient/NASAClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAClientApp.swift
+//  NASAClient
+//
+//  Created by Yunosuke Sakai on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct NASAClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
