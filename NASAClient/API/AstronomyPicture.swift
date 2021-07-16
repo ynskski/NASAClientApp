6a728efd9f23e750ebc9f11387e259a84c5518d6
Yunosuke Sakai <qmkns246@gmail.com>
2021-07-16 21:45:09 +0900

    APODを表すモデルに動画の場合の対応を追加

--- NASAClient/API/AstronomyPicture.swift
+++ NASAClient/API/AstronomyPicture.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 struct AstronomyPicture: Decodable {
-    var copyright: String
+    var copyright: String?
     var date: String
     var explanation: String
-    var hdURL: String
+    var hdURL: String?
     var mediaType: String
     var title: String
     var url: String
