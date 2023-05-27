ec41b7d0c5de29e2a51c73017884205f7334b00e
Yunosuke Sakai <qmkns246@gmail.com>
2023-05-27 18:37:28 +0900

    Add `WebView`

---
+++ NASAClient/Views/WebView.swift
@@ -0,0 +1,27 @@
+//
+//  WebView.swift
+//  NASAClient
+//
+//  Created by YunosukeSakai on 2023/05/27.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView(url: .init(string: "https://www.youtube.com/embed/V_Kd4YBNs7c?rel=0")!)
+    }
+}
