c831ee311e760ac184590c5ee136e5c653fc8423
Yunosuke Sakai <qmkns246@gmail.com>
2024-01-31 22:36:52 +0900

    stop supporting iOS 15

--- Features/Package.swift
+++ Features/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Features",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "API",
