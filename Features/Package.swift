ee3e9ffaa785a301e8914af8d93cee5bc3156d15
Yunosuke Sakai <qmkns246@gmail.com>
2024-05-03 13:45:22 +0900

    update deployment target to 17.0

--- Features/Package.swift
+++ Features/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Features",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "APIClient",
