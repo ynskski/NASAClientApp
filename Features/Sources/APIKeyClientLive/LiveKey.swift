da56b334a6ff07ad33c24c03e9077682f6c2a087
ski-u <37182704+ski-u@users.noreply.github.com>
2024-12-14 15:15:45 +0000

    run swift-format

--- Features/Sources/APIKeyClientLive/LiveKey.swift
+++ Features/Sources/APIKeyClientLive/LiveKey.swift
@@ -1,14 +1,14 @@
 import APIKeyClient
 import Dependencies
-import Models
 @preconcurrency import KeychainAccess
+import Models
 
 extension APIKeyClient: DependencyKey {
     public static let liveValue: Self = .liveValue(service: "dev.ynskski.NASAClient")
-    
+
     static func liveValue(service: String) -> APIKeyClient {
         let keychain = Keychain(service: service)
-        
+
         return .init(
             getKey: {
                 keychain["APIKey"].map(APIKey.init(rawValue:))
--- NASAClientTests/APIKeyClientLive/APIKeyClientLiveTests.swift
+++ NASAClientTests/APIKeyClientLive/APIKeyClientLiveTests.swift
@@ -1,24 +1,25 @@
 import APIKeyClient
-@testable import APIKeyClientLive
-import Models
 import KeychainAccess
+import Models
 import Testing
+
+@testable import APIKeyClientLive
 
 struct APIKeyClientLiveTests {
     let keychain = Keychain(service: "test")
-    
+
     init() {
         try! keychain.removeAll()
     }
-    
+
     @Test
     func liveValue() {
         let client = APIKeyClient.liveValue(service: "test")
         let key = APIKey(rawValue: "key")
-        
+
         client.setKey(key)
         #expect(client.getKey() == key)
-        
+
         client.setKey(nil)
         #expect(client.getKey() == nil)
     }
